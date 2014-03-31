fba663f976a977c89b583a8766b4e7cf82c14ca5
Fernando Serboncini <fserb@fserb.com.br>
2014-03-31 02:07:21 +0200

    Test and doc

--- README.md
+++ README.md
@@ -5,10 +5,13 @@ Haxe port of sfxr.
 
 Based on [Tomas Pettersson's sfxr](http://www.drpetter.se/project_sfxr.html) and [Thomas Vian's as3sfxr](http://www.superflashbros.net/as3sfxr/).
 
+This is a working version for flash and native. See SfxrTest.hx for an example.
+
+
 TODO
 ----
 - Proper documentation and examples
-- Add proper per platform cache
+- Add proper per platform cache (cache final object instead of buffer)
 - Support for html5 target
 - Add better test
 
--- test/src/SfxrTest.hx
+++ test/src/SfxrTest.hx
@@ -12,18 +12,17 @@ class SfxrTest {
   static function main() {
     // new SfxrTest();
     var params = new SfxrParams();
-    params.waveType = 0;
-    params.squareDuty = 0.55555*0.6;
-    params.startFrequency = 0.3 + 0.55555*0.3;
-    params.slide = 0.1 + 0.55555*0.2;
-    params.attackTime = 0.0;
-    params.sustainTime = 0.1 + 0.55555*0.3;
-    params.decayTime = 0.1 + 0.55555*0.2;
-    // if (0.55555 < 0.5) params.hpFilterCutoff = 0.55555*0.3;
-    // if (0.55555 < 0.5) params.lpFilterCutoff = 1.0 - 0.55555*0.6;
 
-    params.masterVolume = 0.15;
+    // params.waveType = 0;
+    // params.squareDuty = 0.55555*0.6;
+    // params.startFrequency = 0.3 + 0.55555*0.3;
+    // params.slide = 0.1 + 0.55555*0.2;
+    // params.attackTime = 0.0;
+    // params.sustainTime = 0.1 + 0.55555*0.3;
+    // params.decayTime = 0.1 + 0.55555*0.2;
+    // params.masterVolume = 0.15;
 
+    // taken from as3sfxr:
     params = SfxrParams.fromString("0,,0.2193,,0.4748,0.3482,,0.0691,,,,,,0.3482,,,,,1,,,,,0.5");
     var sfxr = new Sfxr(params);
     sfxr.play();
